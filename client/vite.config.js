c4db3921a7e36d93fc6981a6896bdb265188a213
Gaurav Ghuge <guduughuge7@gmail.com>
2024-08-07 15:12:52 +0530

    update the content

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': 'https://employeemanagementsystem-copy-production.up.railway.app/',
+      '/api': 'https://employeemanagementsystem-copy-production.up.railway.app',
 
       // '/': 'http://localhost:5200/',
       
