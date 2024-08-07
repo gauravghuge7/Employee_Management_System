8b090fc1594a8a142fe679c44953e0ede4b5b989
Gaurav Ghuge <guduughuge7@gmail.com>
2024-08-07 14:31:06 +0530

    update the backend connectivity to frontend

--- client/src/Url/url.backend.js
+++ client/src/Url/url.backend.js
@@ -1,5 +1,8 @@
 
-export const serverUrl = 'http://localhost:5200';
+// export const serverUrl = 'http://localhost:5200';
+
+
+export const serverUrl = 'https://emplyeemanagement1-production.up.railway.app/' ;
 
 
 
